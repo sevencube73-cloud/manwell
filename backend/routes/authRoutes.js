dd040cd591afa753c34175869d00b7c026e02a34
sevencube73-cloud <sevencube73@gmail.com>
2025-10-11 15:27:54 +0300

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ import { registerUser, loginUser } from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/register', registerUser);
-router.get('/verify-email', verifyEmail);
+
 router.post('/login', loginUser);
 
 export default router;
