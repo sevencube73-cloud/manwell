f5367ef371a0f01a7e0b8816f4f3a3c171efc2ad
Muzamilafey <muzamilinc@gmail.com>
2025-10-16 22:12:47 +0300

    cvdf

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (to, subject, html) => {
   try {
     const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "admin@muzamilafey.digital",
       to,
       subject,
       html,
